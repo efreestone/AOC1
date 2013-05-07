68d2f71c693df61f5f4ad2cba75f708c74e36350
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-05-07 16:08:01 -0600

    Initial commit after creating project 1

---
+++ AOC1Project1/AOC1Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1Project1
+//
+//  Created by Elijah Freestone on 5/7/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
