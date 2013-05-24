4ebc6380cac3b1a159c940d72162bf97c3cc6f6c
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-05-23 19:44:04 -0600

    Added Compare function call with UIAlert. Various cleanup

--- AOC1Project3/AOC1Project3/ViewController.m
+++ AOC1Project3/AOC1Project3/ViewController.m
@@ -61,12 +61,12 @@
     //DONE1. Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
     //DONE2. Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
     //DONE3. Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
-    //DONE4. Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    //--DONE4. Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
     //DONE5. Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
     //DONE6. Call the Add function passing in two integer values. Capture the return of this function into a variable.
-    //DONE7. Bundle the returned integer (from Add)into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    //--DONE7. Bundle the returned integer (from Add)into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
     //???8. Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
-    //9. Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
+    //--DONE9. Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
     
     //DisplayAlertWithString Function
     //1. Take the passed in NSString and display it in the alert view
@@ -76,21 +76,30 @@
     //End of instructions - DELETE FROM FINAL!!
 
     
-    //Call "Add" function and test
-    int myResult = [self Add:2 toInteger:7];
-    NSLog(@"%d out of 10 people love bacon.", myResult);
-    
-    //Call "Compare" function and test
-    BOOL myBool = [self Compare:5 toInteger:5];
-    NSLog(@"Would you like some bacon? %@!", myBool ? @"YES" : @"NO");
-    
     //Call "Append" function and test
     NSString *newString = [self Append:@"Did it work? " toString:@"Yeppers!"];
     //Call displayAlertWithString to pop up UIAlert with newString as text
-    [self displayAlertWithString:newString];
+    if (newString != nil) {
+        [self displayAlertWithString:newString];
+    }
     
-    //Change int myResult into NSNumber before converting to NSString
-    NSNumber *convertNumber = [[NSNumber alloc] initWithInt:myResult];
+    //Define variables and Call "Compare" function, display in UIAlert with displayAlertWithString
+    NSInteger firstNumber = 5;
+    NSInteger secondNumber = 5;
+    BOOL myBool = [self Compare:firstNumber toInteger:secondNumber];
+    //NSLog(@"Would you like some bacon? %@!", myBool ? @"YES" : @"NO");
+    //Convert myBool to an NSString to be displayed in a UIAlertView. %ld to cast NSInteger to long int
+    NSString *boolString = [NSString stringWithFormat:@"Is %ld == %ld? %@", (long)firstNumber, (long)secondNumber, myBool ? @"YES" : @"NO"];
+    //Use displayAlertWithString to display boolString in UIAlert. If statement to avoid error
+    if (firstNumber == secondNumber) {
+        [self displayAlertWithString:boolString];
+    }
+    
+    //Call "Add" function to be converted and displayed with displayAlertWithString
+    int myResult = [self Add:2 toInteger:7];
+    //NSLog(@"%d out of 10 people love bacon.", myResult);
+    //Change int myResult into NSNumber before converting to NSString. Used numberWithInt for mem auto-release
+    NSNumber *convertNumber = [NSNumber numberWithInt:myResult];
     //Convert convertNumber to NSString
     NSString *numString = [convertNumber stringValue];
     //Create message to append convertNumber to
@@ -98,9 +107,11 @@
     //Append numberText to numString
     NSString *appendNumber = [self Append:numberText toString:numString];
     //NSLog(@"%@", appendNumber);
-    //Add appendnumber to displayAlertWithString and display in UIAlert
-    [self displayAlertWithString:appendNumber];
-
+    //Use displayAlertWithString to display appendNumber in UIAlert
+    if (appendNumber != nil) {
+        [self displayAlertWithString:appendNumber];
+    }
+    
 
     
     [super viewDidLoad];
