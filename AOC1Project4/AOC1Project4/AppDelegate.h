c99a244757f64cb006532687b88a19079f71dada
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-05-27 19:16:42 -0600

    Initial commit after creating Project 4 (singleview). Instructions added to viewController.m

---
+++ AOC1Project4/AOC1Project4/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AOC1Project4
+//
+//  Created by Elijah Freestone on 5/27/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC1Project4/AOC1Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1Project4
+//
+//  Created by Elijah Freestone on 5/27/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
